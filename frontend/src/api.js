1b16893e71d23df3b86a9deb1413f12039a0c95c
Sumit Dhaker <sumit006162@gmail.com>
2025-08-31 00:31:43 +0530

    change env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Create axios instance with backend base URL
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL:process.env.baseURL
+  ,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
